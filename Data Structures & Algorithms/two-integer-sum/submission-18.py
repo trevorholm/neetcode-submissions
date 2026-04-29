251f6e7d16bb669840d643bfd00a61b153ef20e9
Trevor Holm <holminator1000@gmail.com>
2026-04-29 13:26:52 -0700

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Space: O(n) Time: O(n)
+        hashMap = {} # Store the value and index of each element
+
+        for i, num in enumerate(nums): # Iterate through the array
+            difference = target - num  
+            if difference in hashMap: # Check if the complement exists in the hash map
+                return [hashMap[difference], i] # Return the indices of the current element and it's complement
+            hashMap[num] = i 
